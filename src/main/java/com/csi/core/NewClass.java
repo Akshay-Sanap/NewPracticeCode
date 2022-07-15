52196140a2e8d27eb0817005941b5154402cf3bc
Akshay <akshaypsanap@gmail.com>
2022-07-15 17:16:01 +0530

    Second Commit

--- src/main/java/com/csi/core/NewClass.java
+++ src/main/java/com/csi/core/NewClass.java
@@ -3,6 +3,8 @@ package com.csi.core;
 public class NewClass {
 	public static void main(String[] args) {
 		System.out.println("Welcome to CSI");
+		
+		System.out.println("Welcome to Nashik");
 	}
 
 }
